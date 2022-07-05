ad757890418439a540f268debeea2517f3bdd484
JC Olamendy <juanc.olamendy@gmail.com>
2022-07-05 10:27:43 -0400

    move conf into env

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,14 @@
 const webpack = require('webpack');
 const path = require('path');
+require("dotenv/config");
 
 module.exports = (env, argv) => {
-  let URL_API  = 'http://localhost:3000/';
-  if(argv.mode === 'production') {
-    URL_API  = 'https://remote.com/';
-  }
-
   console.log("--- Webpack mode:", argv.mode);
-  console.log("--- URL_API:", URL_API);
+  console.log("--- URL_API:", process.env.URL_API);
 
   const plugins = [
     new webpack.DefinePlugin({
-      URL_API:  JSON.stringify(URL_API),
+      URL_API:  JSON.stringify(process.env.URL_API),
     })
   ];
 
